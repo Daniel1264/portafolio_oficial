0152921ef1b3053019b7db76df0cce61ab745cbe
Daniel1264 <94574017+Daniel1264@users.noreply.github.com>
2022-12-21 13:26:49 -0600

    fix proyects querys

--- src/components/body/Proyects.jsx
+++ src/components/body/Proyects.jsx
@@ -55,7 +55,7 @@ const Proyects = () => {
           </div>
 
           <div className="w-full flex flex-wrap justify-center gap-14 items-center">
-            <div className="w-[600px] h-[300px] bg-frontend relative bg-cover">
+            <div className="w-[600px] h-[350px] bg-frontend relative bg-cover">
               <div className="bg-[#0000009c] h-full flex flex-col gap-4 relative p-1">
               <span className="text-3xl text-secondary font-semibold">En proceso</span>
                 <h2 className="text-white font-medium text-5xl">Consulta Medica</h2>
@@ -63,7 +63,7 @@ const Proyects = () => {
                 <SiAltiumdesigner className="absolute text-9xl sm:text-[12em] right-0 top-10   sm:translate-x-20 text-secondary " />
               </div>
             </div>
-            <div className="w-[600px] h-[300px] bg-dark relative bg-cover">
+            <div className="w-[600px] h-[350px] bg-dark relative bg-cover">
               <div className="bg-[#0000009c] h-full flex flex-col gap-4 relative p-1">
               <span className="text-3xl text-secondary font-semibold">En proceso</span>
                 <h2 className="text-white font-medium text-5xl">Red Social</h2>
