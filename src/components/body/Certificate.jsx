987b8702a34580ac5bc72a7535c0854ee000fac0
Daniel1264 <94574017+Daniel1264@users.noreply.github.com>
2022-12-21 13:34:05 -0600

    fix certificate querys components

--- src/components/body/Certificate.jsx
+++ src/components/body/Certificate.jsx
@@ -8,7 +8,7 @@ const Certificate = () => {
       <div className="w-[90%] sm:ml-10 h-full min-h-screen  gap-4 flex justify-center flex-wrap p-3">
 
         <a href='https://certificates.academlo.com/en/verify/08125827091851?ref=email' target='blank'>
-          <div data-aos="fade-up" className="w-[400px] h-full min-h-[650px] bg-setupMovil bg-center relative dark:bg-setup">
+          <div data-aos="fade-up" className="w-[350px] h-full min-h-[650px] bg-setupMovil bg-center relative dark:bg-setup">
             <div className=" absolute right-1 bottom-3">
               <h2 className="text-white text-8xl text-right dark:text-secondary">01</h2>
               <span className="text-white text-2xl">Academlo Frontend With React</span>
@@ -17,7 +17,7 @@ const Certificate = () => {
         </a>
 
         <a href='https://certificates.academlo.com/en/verify/44248802758901?ref=email' target='blank'>
-          <div data-aos="fade-up" className="w-[400px] h-full min-h-[650px]   bg-setupMovil bg-center relative dark:bg-setup">
+          <div data-aos="fade-up" className="w-[350px] h-full min-h-[650px]   bg-setupMovil bg-center relative dark:bg-setup">
             <div className=" absolute right-1 bottom-3">
               <h2 className="text-white text-8xl text-right dark:text-secondary">02</h2>
               <span className="text-white text-2xl">Academlo Fundamentos</span>
@@ -26,7 +26,7 @@ const Certificate = () => {
         </a>
 
         <a href='https://drive.google.com/file/d/1BNfgsLe-CRw1-TW_xXL0F9uP888xqye7/view?usp=share_link' target='blank'>
-          <div data-aos="fade-up" className="w-[400px] h-full min-h-[650px]   bg-setupMovil bg-center relative dark:bg-setup">
+          <div data-aos="fade-up" className="w-[350px] h-full min-h-[650px]   bg-setupMovil bg-center relative dark:bg-setup">
             <div className=" absolute right-1 bottom-3">
               <h2 className="text-white text-8xl text-right dark:text-secondary">03</h2>
               <span className="text-white text-2xl">ArtistCode Linux</span>
@@ -35,7 +35,7 @@ const Certificate = () => {
         </a>
 
         <a href='https://drive.google.com/file/d/1VHO3r_fFttyNpt9298eslSg0xrU4chek/view?usp=share_link' target='blank'>
-          <div data-aos="fade-up" className="w-[400px] h-full min-h-[650px]   bg-setupMovil bg-center relative dark:bg-setup">
+          <div data-aos="fade-up" className="w-[350px] h-full min-h-[650px]   bg-setupMovil bg-center relative dark:bg-setup">
             <div className=" absolute right-1 bottom-3">
               <h2 className="text-white text-8xl text-right dark:text-secondary">04</h2>
               <span className="text-white text-2xl">ArtistCode Máquina Virtual</span>
